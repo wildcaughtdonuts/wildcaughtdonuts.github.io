75a28b7376108cd79664f9893f538bc65606f707
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-05 23:47:57 +0900

    Add files via upload

--- dkapp.js
+++ dkapp.js
@@ -1,12 +1,6 @@
 function openDaumPostcode() {
   new daum.Postcode({
     oncomplete: function(data) {
-      var outputDiv = document.getElementById('output');
-      outputDiv.innerHTML = 
-        'sigunguCode: ' + data.sigunguCode + '<br>' +
-        'bcode: ' + data.bcode + '<br>' +
-        'roadnameCode: ' + data.roadnameCode + '<br>' +
-        'query: ' + data.query;
       document.getElementById('address').value = data.roadAddress;
     }
   }).open();
