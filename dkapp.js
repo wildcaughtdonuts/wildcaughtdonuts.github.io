80eff5a63518963864946bfd5adbc63d5dd8e87c
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-07 01:02:21 +0900

    Add files via upload

--- dkapp.js
+++ dkapp.js
@@ -1,49 +1,60 @@
-document.getElementById("search-btn").addEventListener("click", function() {
+document.getElementById("search-btn").addEventListener("click", function () {
   new daum.Postcode({
-    oncomplete: function(data) {
+    oncomplete: function (data) {
       var buildingCode = data.buildingCode;
       document.getElementById("address-input").value = data.address;
-      document.getElementById("building-code").innerHTML = "건물 코2드: " + buildingCode;
+      document.getElementById("building-code").innerHTML = "건물 코드: " + buildingCode;
 
       var apiKey = "5A1ar8VsZgpiuOpuMbwPSgtsHIl%2FDCfu%2FMINUxKvTbwgL6nXfgG42fYYAHIq4gmp1bUZcQHO%2F1B2ilg7w8Hlzw%3D%3D";
       var siggCd = buildingCode.substr(0, 5);
       var bjdCd = buildingCode.substr(5, 5);
       var bunCd = buildingCode.substr(11, 4);
       var jiCd = buildingCode.substr(15, 4);
-      var apiUrl = "https://apis.data.go.kr/1613000/BldRgstService_v2/getBrRecapTitleInfo?sigunguCd=" + siggCd + "&bjdongCd=" + bjdCd + "&platGbCd=0&bun=" + bunCd + "&ji=" + jiCd + "&ServiceKey=" + apiKey;
-      document.getElementById("api-url").innerHTML = "API URL: " + apiUrl;
 
-      fetch(apiUrl)
-        .then(response => response.text())
-        .then(data => {
-          var parser = new DOMParser();
-          var xmlDoc = parser.parseFromString(data, "text/xml");
-          var item = xmlDoc.getElementsByTagName("item")[0];
-          var itemData = {
-            "건물명": item.getElementsByTagName("bldNm")[0].textContent,
-            "용도": item.getElementsByTagName("etcPurps")[0].textContent,
-            "주소": item.getElementsByTagName("platPlc")[0].textContent,
-            "건축허가일": item.getElementsByTagName("pmsDay")[0].textContent,
-            "사용승인일": item.getElementsByTagName("useAprDay")[0].textContent,
-            "연면적": item.getElementsByTagName("totArea")[0].textContent + "㎡",
-            "건축면적": item.getElementsByTagName("archArea")[0].textContent + "㎡",
-            "세대수": item.getElementsByTagName("hhldCnt")[0].textContent,
-            
-          };
-          var html = "<ul>";
-          for (var key in itemData) {
-            html += "<li>" + key + ": " + itemData[key] + "</li>";
+      var btns = document.querySelectorAll('[data-type]');
+      btns.forEach(function (btn) {
+        btn.addEventListener("click", function () {
+          var type = this.getAttribute("data-type");
+          var apiUrl;
+          if (type === "title") {
+            apiUrl = "https://apis.data.go.kr/1613000/BldRgstService_v2/getBrTitleInfo?sigunguCd=" + siggCd + "&bjdongCd=" + bjdCd + "&platGbCd=0&bun=" + bunCd + "&ji=" + jiCd + "&ServiceKey=" + apiKey;
+          } else if (type === "recap") {
+            apiUrl = "https://apis.data.go.kr/1613000/BldRgstService_v2/getBrRecapTitleInfo?sigunguCd=" + siggCd + "&bjdongCd=" + bjdCd + "&platGbCd=0&bun=" + bunCd + "&ji=" + jiCd + "&ServiceKey=" + apiKey;
           }
-          html += "</ul>";
-          document.getElementById("api-data").innerHTML = "API 정보:" + html;
-        })
-        .catch(error => {
-          console.error(error);
-          document.getElementById("api-data").innerHTML = "API 정보를 가져오는 중 오류가 발생했습니다.";
+          document.getElementById("api-url").innerHTML = "API URL: " + apiUrl;
+          
+          // API 호출 및 데이터 출력
+          fetch(apiUrl)
+            .then(response => response.text())
+            .then(str => {
+              const parser = new DOMParser();
+              const xmlDoc = parser.parseFromString(str, "text/xml");
+              const item = xmlDoc.getElementsByTagName("item")[0];
+              const itemData = {
+                "건물명": item.getElementsByTagName("bldNm")[0].textContent,
+                "용도": item.getElementsByTagName("etcPurps")[0].textContent,
+                "주소": item.getElementsByTagName("platPlc")[0].textContent,
+                "건축허가일": item.getElementsByTagName("pmsDay")[0].textContent,
+                "사용승인일": item.getElementsByTagName("useAprDay")[0].textContent,
+                "연면적": item.getElementsByTagName("totArea")[0].textContent + "㎡",
+                "건축면적": item.getElementsByTagName("archArea")[0].textContent + "㎡",
+                "세대수": item.getElementsByTagName("hhldCnt")[0].textContent,
+              };
+              let html = "<ul>";
+              for (let key in itemData) {
+                html += `<li>${key}: ${itemData[key]}</li>`;
+              }
+              html += "</ul>";
+              document.getElementById("api-data").innerHTML = `API 정보: ${html}`;
+            })
+            .catch(error => {
+              console.error(error);
+              document.getElementById("api-data").innerHTML = "API 정보를 가져오는 중 오류가 발생했습니다.";
+            });
+
+
         });
-
-
-
+      });
     }
   }).open();
 });
