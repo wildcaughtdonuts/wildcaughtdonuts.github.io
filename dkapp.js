e09cf5e56256aea0e05029b30a77201b136cbe7d
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-06 22:11:30 +0900

    Add files via upload

--- dkapp.js
+++ dkapp.js
@@ -14,38 +14,28 @@ document.getElementById("search-btn").addEventListener("click", function() {
       document.getElementById("api-url").innerHTML = "API URL: " + apiUrl;
       
       fetch(apiUrl)
-  .then(response => response.text())
-  .then(data => {
-    var parser = new DOMParser();
-    var xmlDoc = parser.parseFromString(data, "text/xml");
-    var items = xmlDoc.getElementsByTagName("item")[0];
-    var itemData = {};
-    if (items.getElementsByTagName("mainPurpsCdNm").length > 0) {
-      itemData["주용도"] = items.getElementsByTagName("mainPurpsCdNm")[0].textContent;
-    }
-    if (items.getElementsByTagName("purpsCdNm").length > 0) {
-      itemData["용도"] = items.getElementsByTagName("purpsCdNm")[0].textContent;
-    }
-    if (items.getElementsByTagName("bcRgstKindCdNm").length > 0) {
-      itemData["구조"] = items.getElementsByTagName("bcRgstKindCdNm")[0].textContent;
-    }
-    if (items.getElementsByTagName("archArea").length > 0) {
-      itemData["면적"] = items.getElementsByTagName("archArea")[0].textContent + "㎡";
-    }
-    if (items.getElementsByTagName("hhldCnt").length > 0) {
-      itemData["층수"] = items.getElementsByTagName("hhldCnt")[0].textContent + "층";
-    }
-    var html = "<ul>";
-    for (var key in itemData) {
-      html += "<li>" + key + ": " + itemData[key] + "</li>";
-    }
-    html += "</ul>";
-    document.getElementById("api-data").innerHTML = "API 정보:" + html;
-  })
-  .catch(error => {
-    console.error(error);
-    document.getElementById("api-data").innerHTML = "API 정보를 가져오는 중 오류가 발생했습니다.";
-  });
+        .then(response => response.json())
+        .then(data => {
+          var item = data.response.body.items[0];
+          var itemData = {
+            "주용도": item.mainPurpsCdNm,
+            "용도": item.purpsCdNm,
+            "구조": item.bcRgstKindCdNm,
+            "면적": item.archArea + "㎡",
+            "층수": item.hhldCnt + "층"
+          };
+          var html = "<ul>";
+          for (var key in itemData) {
+            html += "<li>" + key + ": " + itemData[key] + "</li>";
+          }
+          html += "</ul>";
+          document.getElementById("api-data").innerHTML = "API 정보:" + html;
+        })
+        .catch(error => {
+          console.error(error);
+          document.getElementById("api-data").innerHTML = "API 정보를 가져오는 중 오류가 발생했습니다.";
+        });
+
 
     }
   }).open();
