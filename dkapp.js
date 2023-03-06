ea1e9acbeb0eaf490806e2c93d84e7c0e35fa86c
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-06 22:42:00 +0900

    Add files via upload

--- dkapp.js
+++ dkapp.js
@@ -4,15 +4,15 @@ document.getElementById("search-btn").addEventListener("click", function() {
       var buildingCode = data.buildingCode;
       document.getElementById("address-input").value = data.address;
       document.getElementById("building-code").innerHTML = "건물 코드: " + buildingCode;
-      
+
       var apiKey = "5A1ar8VsZgpiuOpuMbwPSgtsHIl%2FDCfu%2FMINUxKvTbwgL6nXfgG42fYYAHIq4gmp1bUZcQHO%2F1B2ilg7w8Hlzw%3D%3D";
       var siggCd = buildingCode.substr(0, 5);
       var bjdCd = buildingCode.substr(5, 5);
       var bunCd = buildingCode.substr(11, 4);
       var jiCd = buildingCode.substr(15, 4);
-      var apiUrl = "https://apis.data.go.kr/1613000/BldRgstService_v2/getBrBasisOulnInfo?sigunguCd=" + siggCd + "&bjdongCd=" + bjdCd + "&platGbCd=0&bun=" + bunCd + "&ji=" + jiCd + "&ServiceKey=" + apiKey;
+      var apiUrl = "https://apis.data.go.kr/1613000/BldRgstService_v2/getBrRecapTitleInfo?sigunguCd=" + siggCd + "&bjdongCd=" + bjdCd + "&platGbCd=0&bun=" + bunCd + "&ji=" + jiCd + "&ServiceKey=" + apiKey;
       document.getElementById("api-url").innerHTML = "API URL: " + apiUrl;
-      
+
       fetch(apiUrl)
         .then(response => response.text())
         .then(data => {
@@ -20,11 +20,13 @@ document.getElementById("search-btn").addEventListener("click", function() {
           var xmlDoc = parser.parseFromString(data, "text/xml");
           var item = xmlDoc.getElementsByTagName("item")[0];
           var itemData = {
-            "주용도": item.getElementsByTagName("mainPurpsCdNm")[0].textContent,
-            "용도": item.getElementsByTagName("purpsCdNm")[0].textContent,
-            "구조": item.getElementsByTagName("bcRgstKindCdNm")[0].textContent,
-            "면적": item.getElementsByTagName("archArea")[0].textContent + "㎡",
-            "층수": item.getElementsByTagName("hhldCnt")[0].textContent + "층"
+            "건물명": item.getElementsByTagName("bldNm")[0].textContent,
+            "주소": item.getElementsByTagName("platPlc")[0].textContent,
+            "용도": item.getElementsByTagName("etcPurps")[0].textContent,
+            "세대수": item.getElementsByTagName("hhldCnt")[0].textContent,
+            "연면적": item.getElementsByTagName("totArea")[0].textContent + "㎡",
+            "허가일": item.getElementsByTagName("pmsDay")[0].textContent,
+            "사용승인일": item.getElementsByTagName("useAprDay")[0].textContent
           };
           var html = "<ul>";
           for (var key in itemData) {
