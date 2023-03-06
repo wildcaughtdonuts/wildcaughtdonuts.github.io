cc072795d53585cfc026673535ab382dd994ba51
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-06 18:00:09 +0900

    Add files via upload

--- dkapp.js
+++ dkapp.js
@@ -3,21 +3,21 @@ function openDaumPostcode() {
     oncomplete: function(data) {
       document.getElementById('address').value = data.roadAddress;
 
-      // 검색 결과 데이터 추출
-      const sigunguCode = data.sigunguCode;
-      const bcode = data.bcode;
-      const roadnameCode = data.roadnameCode;
-      const query = data.query;
-
       // 건축물대장 API 요청
+      const buildingCode = data.buildingCode;
+      const sggCd = buildingCode.substring(0, 5);
+      const bjdCd = buildingCode.substring(5, 10);
+      const bunCd = buildingCode.substring(10, 15);
+      const jiCd = buildingCode.substring(15, 20);
+
       const xhr = new XMLHttpRequest();
-      const url = `http://apis.data.go.kr/1613000/BldRgstService_v2/getBrBasisOulnInfo?sigunguCd=${sigunguCode}&bjdongCd=${bcode}&platGbCd=0&bun=${query}&ji=&ServiceKey=5A1ar8VsZgpiuOpuMbwPSgtsHIl%2FDCfu%2FMINUxKvTbwgL6nXfgG42fYYAHIq4gmp1bUZcQHO%2F1B2ilg7w8Hlzw%3D%3D`;
+      const url = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrBasisOulnInfo?sigunguCd=${sggCd}&bjdongCd=${bjdCd}&platGbCd=0&bun=${bunCd}&ji=${jiCd}&ServiceKey=5A1ar8VsZgpiuOpuMbwPSgtsHIl%2FDCfu%2FMINUxKvTbwgL6nXfgG42fYYAHIq4gmp1bUZcQHO%2F1B2ilg7w8Hlzw%3D%3D`;
 
       xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
           if (xhr.status === 200) {
             // API 응답 결과 처리
-            const result = document.getElementById('result');
+            const result = document.createElement('div');
             const responseXML = xhr.responseXML;
             const platLoc = responseXML.getElementsByTagName('platLoc')[0].childNodes[0].nodeValue;
             const bldNm = responseXML.getElementsByTagName('bldNm')[0].childNodes[0].nodeValue;
@@ -26,6 +26,10 @@ function openDaumPostcode() {
             result.innerHTML = `주소: ${platLoc}<br>`;
             result.innerHTML += `건물명: ${bldNm}<br>`;
             result.innerHTML += `주용도: ${mainPurpsCdNm}`;
+
+            const target = document.getElementById('result');
+            target.innerHTML = '';
+            target.appendChild(result);
           } else {
             console.error(xhr.statusText);
           }
