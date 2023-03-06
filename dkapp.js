bd21b1d92d60cb157db77101433e9de972ea84ae
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-06 18:24:57 +0900

    Add files via upload

--- dkapp.js
+++ dkapp.js
@@ -8,7 +8,7 @@ function openDaumPostcode() {
       const sggCd = buildingCode.substring(0, 5);
       const bjdCd = buildingCode.substring(5, 10);
       const bunCd = buildingCode.substring(11, 15);
-      const jiCd = buildingCode.substring(16, 20);
+      const jiCd = buildingCode.substring(15, 19);
 
       const xhr = new XMLHttpRequest();
       const url = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrBasisOulnInfo?sigunguCd=${sggCd}&bjdongCd=${bjdCd}&platGbCd=0&bun=${bunCd}&ji=${jiCd}&ServiceKey=5A1ar8VsZgpiuOpuMbwPSgtsHIl%2FDCfu%2FMINUxKvTbwgL6nXfgG42fYYAHIq4gmp1bUZcQHO%2F1B2ilg7w8Hlzw%3D%3D`;
@@ -17,19 +17,20 @@ function openDaumPostcode() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
           if (xhr.status === 200) {
             const result = document.createElement('div');
-            const responseXML = xhr.responseXML;
-            const platLoc = responseXML.getElementsByTagName('platLoc')[0]?.childNodes[0]?.nodeValue;
-            const bldNm = responseXML.getElementsByTagName('bldNm')[0]?.childNodes[0]?.nodeValue;
-            const mainPurpsCdNm = responseXML.getElementsByTagName('mainPurpsCdNm')[0]?.childNodes[0]?.nodeValue;
-            
-            if (platLoc && bldNm && mainPurpsCdNm) {
+            const responseJSON = JSON.parse(xhr.responseText);
+
+            if (!responseJSON.response.body.items) {
+              result.innerHTML = '건물 대장 정보가 없습니다. <br> API 요청 주소: ' + url;
+            } else {
+              const platLoc = responseJSON.response.body.items.item.platLoc;
+              const bldNm = responseJSON.response.body.items.item.bldNm;
+              const mainPurpsCdNm = responseJSON.response.body.items.item.mainPurpsCdNm;
+
               result.innerHTML = `주소: ${platLoc}<br>`;
               result.innerHTML += `건물명: ${bldNm}<br>`;
               result.innerHTML += `주용도: ${mainPurpsCdNm}`;
-            } else {
-              result.innerHTML = "건물 대장 정보가 없습니다.";
             }
-            
+
             const target = document.getElementById('result');
             target.innerHTML = '';
             target.appendChild(result);
@@ -38,8 +39,7 @@ function openDaumPostcode() {
           }
         }
       };
-      
-      
+
       xhr.open('GET', url, true);
       xhr.send();
     }
