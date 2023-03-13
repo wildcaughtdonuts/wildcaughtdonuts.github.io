c09f2dcf748e14d56742de07201ec3b4be128377
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-13 23:11:53 +0900

    Update app.js

--- buildingBoss/app.js
+++ buildingBoss/app.js
@@ -4,16 +4,54 @@ const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 const urlInput = document.getElementById('url-input');
 
+const unitNames = {
+  'main': '건축물 대장 정보',
+  'bld': '건축물 기본정보',
+  'use': '건축물 사용승인 정보',
+  'apr': '건축물 허가 정보'
+};
+
+const itemNames = {
+  'bldNm': '건물명',
+  'mainPurpsCdNm': '주용도',
+  'etcPurps': '기타용도',
+  'totArea': '연면적(㎡)',
+  'archArea': '건축면적(㎡)',
+  'hhldCnt': '세대수',
+  'pmsDay': '처리일자',
+  'useAprDay': '사용승인일자'
+};
+
 submitBtn.addEventListener('click', () => {
   const url = urlInput.value;
 
   fetch(url)
-    .then(response => response.text())
-    .then(data => {
-      resultDiv.innerHTML = data;
-    })
-    .catch(error => {
-      resultDiv.innerHTML = '오류가 발생했습니다. URL을 확인해주세요.';
-      console.error(error);
-    });
+  .then(response => response.text())
+  .then(data => {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(data, 'text/xml');
+    const items = xmlDoc.getElementsByTagName('item');
+    
+    let resultHTML = '';
+    
+    for (let unit in unitNames) {
+      resultHTML += `<h3>${unitNames[unit]}</h3><ul>`;
+      const itemElements = xmlDoc.getElementsByTagName(unit)[0].children;
+      for (let i = 0; i < itemElements.length; i++) {
+        const itemElement = itemElements[i];
+        const itemName = itemElement.tagName;
+        const itemValue = itemElement.textContent || '정보없음';
+        if (itemNames[itemName]) {
+          resultHTML += `<li><strong>${itemNames[itemName]}:</strong> ${itemValue}</li>`;
+        }
+      }
+      resultHTML += '</ul>';
+    }
+
+    resultDiv.innerHTML = resultHTML;
+  })
+  .catch(error => {
+    resultDiv.innerHTML = '오류가 발생했습니다. URL을 확인해주세요.';
+    console.error(error);
+  });
 });
