7510d5ed4512cebf5d7c5fa953a5f570911e511d
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-13 23:28:06 +0900

    Add files via upload

--- buildingBoss/app.js
+++ buildingBoss/app.js
@@ -10,7 +10,38 @@ submitBtn.addEventListener('click', () => {
   fetch(url)
     .then(response => response.text())
     .then(data => {
-      resultDiv.innerHTML = data;
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, 'text/xml');
+      const items = xmlDoc.getElementsByTagName('item');
+      let itemInfo = {};
+
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const bldNm = item.getElementsByTagName('bldNm')[0]?.textContent || '건축물명 없음';
+        const archArea = parseInt(item.getElementsByTagName('archArea')[0]?.textContent || 0);
+        itemInfo[bldNm] = {
+          '주용도': item.getElementsByTagName('mainPurpsCdNm')[0]?.textContent || '정보없음',
+          '기타용도': item.getElementsByTagName('etcPurps')[0]?.textContent || '정보없음',
+          '연면적(㎡)': item.getElementsByTagName('totArea')[0]?.textContent || '정보없음',
+          '세대수': item.getElementsByTagName('hhldCnt')[0]?.textContent || '정보없음',
+          '처리일자': item.getElementsByTagName('pmsDay')[0]?.textContent || '정보없음',
+          '사용승인일자': item.getElementsByTagName('useAprDay')[0]?.textContent || '정보없음'
+        };
+        if (!isNaN(archArea)) {
+          itemInfo[bldNm]['건축면적(㎡)'] = archArea;
+        }
+      }
+
+      let resultHTML = '';
+      for (const [bldNm, info] of Object.entries(itemInfo)) {
+        resultHTML += `<h3>${bldNm}</h3><ul>`;
+        for (const [key, value] of Object.entries(info)) {
+          resultHTML += `<li><strong>${key}:</strong> ${value}</li>`;
+        }
+        resultHTML += '</ul>';
+      }
+
+      resultDiv.innerHTML = resultHTML;
     })
     .catch(error => {
       resultDiv.innerHTML = '오류가 발생했습니다. URL을 확인해주세요.';
