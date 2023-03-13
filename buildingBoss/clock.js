bfbce839a439962fa04c7eaec8a2a719d1ab790b
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-13 22:10:10 +0900

    Add files via upload

---
+++ buildingBoss/clock.js
@@ -0,0 +1,3 @@
+const updatedTime = new Date(document.lastModified);
+const updatedTimeDiv = document.getElementById('updated-time');
+updatedTimeDiv.innerText = `Last updated: ${updatedTime.toLocaleString()}`;
--- buildingBoss/script.js
+++ buildingBoss/script.js
@@ -5,48 +5,13 @@ const urlInput = document.getElementById('url-input');
 submitBtn.addEventListener('click', () => {
   const url = urlInput.value;
 
-  // API 요청 보내기
   fetch(url)
     .then(response => response.text())
     .then(data => {
-      // XML 파싱하기
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(data, 'text/xml');
-
-      // 결과를 Unit 종류별로 분류하기
-      const units = new Set();
-      const results = {};
-
-      for (const node of xmlDoc.getElementsByTagName('item')) {
-        const unit = node.getElementsByTagName('bldNm')[0].textContent;
-        if (!units.has(unit)) {
-          units.add(unit);
-          results[unit] = [];
-        }
-        results[unit].push(node);
-      }
-
-      // 결과를 테이블로 표시하기
-      resultDiv.innerHTML = '';
-      for (const unit of units) {
-        const table = document.createElement('table');
-        const caption = document.createElement('caption');
-        caption.textContent = `Unit: ${unit}`;
-        table.appendChild(caption);
-
-        for (const node of results[unit]) {
-          const row = document.createElement('tr');
-          const col1 = document.createElement('td');
-          const col2 = document.createElement('td');
-          col1.textContent = `${node.getElementsByTagName('sidoNm')[0].textContent} ${node.getElementsByTagName('sigunguNm')[0].textContent} ${node.getElementsByTagName('bjdongNm')[0].textContent} ${node.getElementsByTagName('bun')[0].textContent}-${node.getElementsByTagName('ji')[0].textContent}`;
-          col2.textContent = node.getElementsByTagName('bldNm')[0].textContent;
-          row.appendChild(col1);
-          row.appendChild(col2);
-          table.appendChild(row);
-        }
-
-        resultDiv.appendChild(table);
-      }
+      resultDiv.innerHTML = data;
     })
-    .catch(error => console.error(error));
+    .catch(error => {
+      resultDiv.innerHTML = '오류가 발생했습니다. URL을 확인해주세요.';
+      console.error(error);
+    });
 });
