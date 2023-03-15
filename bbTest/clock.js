204fd14c33f69116aea5c82112834c6fb809cab5
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-15 22:50:49 +0900

    Add files via upload

--- bbTest/clock.js
+++ bbTest/clock.js
@@ -3,3 +3,9 @@
 const updatedTime = new Date(document.lastModified);
 const updatedTimeDiv = document.getElementById('updated-time');
 updatedTimeDiv.innerText = `Last updated: ${updatedTime.toLocaleString()}`;
+
+//구글 아날리틱스
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-RT7Z90C0RR');
