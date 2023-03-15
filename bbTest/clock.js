6ae363aae624476e9cd716cbef85b4b20612690b
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-03-15 23:05:39 +0900

    Update clock.js

--- bbTest/clock.js
+++ bbTest/clock.js
@@ -4,8 +4,10 @@ const updatedTime = new Date(document.lastModified);
 const updatedTimeDiv = document.getElementById('updated-time');
 updatedTimeDiv.innerText = `Last updated: ${updatedTime.toLocaleString()}`;
 
-//구글 아날리틱스
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-RT7Z90C0RR');
+// Google Analytics
+document.addEventListener('DOMContentLoaded', () => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'G-RT7Z90C0RR');
+});
