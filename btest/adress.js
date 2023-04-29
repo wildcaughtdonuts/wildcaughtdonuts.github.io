8c190b5df020dec63a4f9e39a33de72b0f651334
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-04-29 23:08:41 +0900

    Add files via upload

--- btest/adress.js
+++ btest/adress.js
@@ -3,9 +3,24 @@
 const searchBtn = document.getElementById('search-btn');
 const mapResultDiv = document.getElementById('mapResult');
 
+function displayMarker(latitude, longitude) {
+  var latLng = new kakao.maps.LatLng(latitude, longitude);
+
+  var marker = new kakao.maps.Marker({
+    position: latLng,
+  });
+
+  marker.setMap(map);
+  map.setCenter(latLng);
+}
+
 searchBtn.addEventListener('click', () => {
   new daum.Postcode({
     oncomplete: function (data) {
+      const lat = data.lat;
+      const lng = data.lng;
+      displayMarker(lat, lng);
+
       const buildingCode = data.buildingCode;
       const apiKey = "5A1ar8VsZgpiuOpuMbwPSgtsHIl%2FDCfu%2FMINUxKvTbwgL6nXfgG42fYYAHIq4gmp1bUZcQHO%2F1B2ilg7w8Hlzw%3D%3D";
       const siggCd = buildingCode.substr(0, 5);
@@ -15,9 +30,8 @@ searchBtn.addEventListener('click', () => {
       const apiUrl = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrTitleInfo?sigunguCd=${siggCd}&bjdongCd=${bjdCd}&platGbCd=0&bun=${bunCd}&ji=${jiCd}&ServiceKey=${apiKey}&numOfRows=150`;
       const recapUrl = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrRecapTitleInfo?sigunguCd=${siggCd}&bjdongCd=${bjdCd}&platGbCd=0&bun=${bunCd}&ji=${jiCd}&ServiceKey=${apiKey}`;
       const flrUrl = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrFlrOulnInfo?sigunguCd=${siggCd}&bjdongCd=${bjdCd}&platGbCd=0&bun=${bunCd}&ji=${jiCd}&ServiceKey=${apiKey}`;
-      // mapResultDiv.innerHTML = `<p>주소 불러오기에 성공했습니다.<br>아래 버튼을 눌러주세요.</p>`;
-      // apiUrl을 저장하고 있는 urlInput 엘리먼트에 apiUrl 저장
-      mapResultDiv.classList.remove('hidden'); 
+
+      mapResultDiv.classList.remove('hidden');
     }
   }).open();
 });
