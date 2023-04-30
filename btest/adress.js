60bfe9f8bea5f3d484225ab1c7c42a2231baabde
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-05-01 00:15:49 +0900

    Add files via upload

--- btest/adress.js
+++ btest/adress.js
@@ -2,10 +2,34 @@
 
 const searchBtn = document.getElementById('search-btn');
 const mapResultDiv = document.getElementById('mapResult');
+const mapContainer = document.getElementById('map');
+
+function showMapWithAddress(address) {
+  var geocoder = new daum.maps.services.Geocoder();
+
+  geocoder.addressSearch(address, function (result, status) {
+    if (status === daum.maps.services.Status.OK) {
+      var coords = new daum.maps.LatLng(result[0].y, result[0].x);
+
+      var mapOption = {
+        center: coords,
+        level: 3,
+      };
+
+      var map = new daum.maps.Map(mapContainer, mapOption);
+
+      var marker = new daum.maps.Marker({
+        map: map,
+        position: coords,
+      });
+    }
+  });
+}
 
 searchBtn.addEventListener('click', () => {
   new daum.Postcode({
     oncomplete: function (data) {
+      showMapWithAddress(data.address);
       const buildingCode = data.buildingCode;
       const apiKey = "imXssiU8dEJ91x2cVSMl3TSW97VrK7cZpGXX5k9pEWgXyzuqmIAwpi9WTa29qcJek2OvrRClAXw0HrzKAlxIhg%3D%3D";
       const siggCd = buildingCode.substr(0, 5);
@@ -15,6 +39,9 @@ searchBtn.addEventListener('click', () => {
       const apiUrl = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrTitleInfo?sigunguCd=${siggCd}&bjdongCd=${bjdCd}&platGbCd=0&bun=${bunCd}&ji=${jiCd}&ServiceKey=${apiKey}`;
       const recapUrl = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrRecapTitleInfo?sigunguCd=${siggCd}&bjdongCd=${bjdCd}&platGbCd=0&bun=${bunCd}&ji=${jiCd}&ServiceKey=${apiKey}`;
       const flrUrl = `https://apis.data.go.kr/1613000/BldRgstService_v2/getBrFlrOulnInfo?sigunguCd=${siggCd}&bjdongCd=${bjdCd}&platGbCd=0&bun=${bunCd}&ji=${jiCd}&ServiceKey=${apiKey}`;
+      
+      
+      
       mapResultDiv.innerHTML = `<p>주소 불러오기에 성공했습니다.<br>아래 버튼을 눌러주세요.</p>`;
       
       document.getElementById('url-input').value = apiUrl;
