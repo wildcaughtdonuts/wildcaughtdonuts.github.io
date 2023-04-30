4fee75a39897f7549cd58e8d2da938fa1d573a83
wildcaughtdonuts <123785671+wildcaughtdonuts@users.noreply.github.com>
2023-04-30 20:53:26 +0900

    Add files via upload

--- btest/app.js
+++ btest/app.js
@@ -422,6 +422,8 @@ submitBtn3.addEventListener("click", () => {
           }
         }
       }
+
+      createAccordionMenu();
     })
     .catch((error) => {
       resultDiv.innerHTML = "오류가 발생했습니다. 다시 시도해주세요.";
